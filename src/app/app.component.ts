900c2f9d3da84a2c3d98dc89dd0b2a005dfbb298
fuuudgeicecream <hipedurban@gmail.com>
2017-04-25 15:33:29 +0200

    Fixing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // Zoom level
+  zoom: number = 10;
+  // Start Position
+  lat: number = 42.858217;
+  lng: number = -70.929990;
+  // Markers
+  markers: marker[] = [
+    {
+        name:'Company One',
+        lat: 42.825588,
+        lng: -71.018029,
+        draggable: true
+    },
+    {
+        name: 'Company Two',
+        lat: 42.868164,
+        lng: -70.889071,
+        draggable: true
+    },
+    {
+        name: 'Company Three',
+        lat: 42.858279,
+        lng: -70.930498,
+        draggable: false
+    }
+  ];
+  
+  constructor(){
+    
+  }
 }
+
+ // Marker Type
+  interface marker{
+    name?:string;
+    lat: number;
+    lng: number;
+    draggable:boolean;
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,6 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyACyIjlKuoBuM0Jk6TsBSf6TGpN6GTZ1yU'
+    }),
     HttpModule
   ],
   providers: [],
